fc251c1a875c44125cd36369158fa594005d7c6c
sgrebnov <v-segreb@microsoft.com>
2015-04-23 16:32:59 -0700

    Added missing copyrights

--- sample/js/index.js
+++ sample/js/index.js
@@ -1,27 +1,7 @@
-﻿/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var AuthenticationContext;
+﻿
+// Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
-//var authority = 'https://login.windows.net/sampleDirectory2015.onmicrosoft.com';
-//var resourceUrl = 'https://graph.windows.net/';
-//var appId = '98ba0820-f7da-4411-87bb-598e0475536b';
-//var redirectUrl = 'http://localhost:4400/services/aad/redirectTarget.html';
+var AuthenticationContext;
 
 var authority = 'https://login.windows.net/test353.onmicrosoft.com';
 var resourceUrl = 'https://graph.windows.net/';
--- scripts/configureEntitlementsIos.js
+++ scripts/configureEntitlementsIos.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
 var CODE_SIGN_ENTITLEMENTS = 'CODE_SIGN_ENTITLEMENTS';
 
 var ACTION_INSTALL = 1;
