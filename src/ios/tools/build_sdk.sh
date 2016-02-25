e1eae7481e8323da969fbdb67815be1ec1a489c4
sgrebnov <v-segreb@microsoft.com>
2016-02-25 11:47:27 +0300

    ios: improves native sdk generation script

--- src/ios/tools/build_sdk.sh
+++ src/ios/tools/build_sdk.sh
@@ -11,11 +11,12 @@ PROJECTS_TO_BUILD=(ADALiOS)
 for i in "${PROJECTS_TO_BUILD[@]}"
 do
 	proj="${i}"
-	echo "Building $proj" 
+	echo "Building $proj"
 	xcodebuild -workspace ADALiOS.xcworkspace -scheme $proj -configuration $BUILD_CONFIGURATION ARCHS="i386 x86_64" -sdk iphonesimulator VALID_ARCHS="i386 x86_64" ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR="../build/emulator" clean build
 	xcodebuild -workspace ADALiOS.xcworkspace -scheme $proj -configuration $BUILD_CONFIGURATION ARCHS="armv7 armv7s arm64" -sdk iphoneos VALID_ARCHS="armv7 armv7s arm64" CONFIGURATION_BUILD_DIR="../build/device" clean build
 	echo "Creating universal version of $proj"
 	rm -rf "$BUILD_PATH/$proj.framework"
+	# Initial framework structure (to be updated later)
 	cp -R "$BUILD_PATH/emulator/$proj.framework" "$BUILD_PATH/$proj.framework"
 
 	simulatorLibPath="$BUILD_PATH/emulator/lib$proj.a"
@@ -25,5 +26,12 @@ do
 	lipo "$simulatorLibPath" "$deviceLibPath" -create -output "$universalLibPath"
 	lipo -info "$universalLibPath"
 done
+
+# Build ADALiOS.bundle (temporary disabled as it does not work for some reason, required files are linked via <resource-file>)
+#xcodebuild -workspace ADALiOS.xcworkspace -scheme ADALiOSBundle clean build CONFIGURATION_BUILD_DIR="../build/"
+
+# Update generated framework (use universal lib version and add missing resources)
+cp -R "$BUILD_PATH/libADALiOS.a" "$BUILD_PATH/$proj.framework/Versions/A/ADALiOS"
+#cp -R "$BUILD_PATH/ADALiOS.bundle" "$BUILD_PATH/$proj.framework/Versions/A/Resources/ADALiOS.bundle"
 
 echo "Done. Build artifacts could be found at '$BUILD_PATH' folder"
