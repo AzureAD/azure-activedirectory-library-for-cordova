7dac7d81ab9c8674ff90fa675b99bfadcfbdcd47
sgrebnov <sergei.grebnov@gmail.com>
2016-05-13 13:43:36 +0300

    Add AppVeyor configuration
    * Android
    * Windows 8.1 Desktop
    * Windows 10 Desktop
    * Windows Phone 8.1

---
+++ tests/paramedic-configs/.paramedic.config.wp81.js
@@ -0,0 +1,11 @@
+
+// Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+module.exports = {
+    "plugins": [
+        "./"
+    ],
+    "platform": "windows",
+    "action": "build",
+    "args": "--archs=arm -- --appx=8.1-phone"
+}
