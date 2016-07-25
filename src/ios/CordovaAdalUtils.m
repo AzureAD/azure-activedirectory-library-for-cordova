6284e4950e5a6c84dd8f22f6a1a6bc5f2e10251d
sgrebnov <sergei.grebnov@gmail.com>
2016-07-25 13:54:46 +0300

    Report error code on iOS

--- src/ios/CordovaAdalUtils.m
+++ src/ios/CordovaAdalUtils.m
@@ -43,7 +43,7 @@
 + (NSMutableDictionary *)ADAuthenticationErrorToDictionary:(ADAuthenticationError *)obj
 {
     NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:1];
-    [dict setObject:[NSNumber numberWithInteger: obj.code] forKey:@"errorCode"];
+    [dict setObject:ObjectOrNull(stringForADErrorCode(obj.code)) forKey:@"errorCode"];
     [dict setObject:ObjectOrNull(obj.protocolCode) forKey:@"error"];
     [dict setObject:ObjectOrNull(obj.errorDetails) forKey:@"errorDescription"];
     return dict;
@@ -103,6 +103,46 @@ static id ObjectOrNull(id object)
         }
     }
     return userId;
+}
+
+static NSString* stringForADErrorCode(NSInteger code)
+{
+    // See https://github.com/AzureAD/azure-activedirectory-library-for-objc/blob/dev/ADAL/src/public/ADErrorCodes.h
+    NSDictionary* errorCodes = @{
+                                 [NSNumber numberWithInt:0]:    @"AD_ERROR_SUCCEEDED",
+                                 [NSNumber numberWithInt:-1]:   @"AD_ERROR_UNEXPECTED",
+                                 [NSNumber numberWithInt:100]:  @"AD_ERROR_DEVELOPER_INVALID_ARGUMENT",
+                                 [NSNumber numberWithInt:101]:  @"AD_ERROR_DEVELOPER_AUTHORITY_VALIDATION",
+                                 [NSNumber numberWithInt:200]:  @"AD_ERROR_SERVER_USER_INPUT_NEEDED",
+                                 [NSNumber numberWithInt:201]:  @"AD_ERROR_SERVER_WPJ_REQUIRED",
+                                 [NSNumber numberWithInt:202]:  @"AD_ERROR_SERVER_OAUTH",
+                                 [NSNumber numberWithInt:203]:  @"AD_ERROR_SERVER_REFRESH_TOKEN_REJECTED",
+                                 [NSNumber numberWithInt:204]:  @"AD_ERROR_SERVER_WRONG_USER",
+                                 [NSNumber numberWithInt:205]:  @"AD_ERROR_SERVER_NON_HTTPS_REDIRECT",
+                                 [NSNumber numberWithInt:206]:  @"AD_ERROR_SERVER_INVALID_ID_TOKEN",
+                                 [NSNumber numberWithInt:207]:  @"AD_ERROR_SERVER_MISSING_AUTHENTICATE_HEADER",
+                                 [NSNumber numberWithInt:208]:  @"AD_ERROR_SERVER_AUTHENTICATE_HEADER_BAD_FORMAT",
+                                 [NSNumber numberWithInt:209]:  @"AD_ERROR_SERVER_UNAUTHORIZED_CODE_EXPECTED",
+                                 [NSNumber numberWithInt:210]:  @"AD_ERROR_SERVER_UNSUPPORTED_REQUEST",
+                                 [NSNumber numberWithInt:211]:  @"AD_ERROR_SERVER_AUTHORIZATION_CODE",
+                                 [NSNumber numberWithInt:300]:  @"AD_ERROR_CACHE_MULTIPLE_USERS",
+                                 [NSNumber numberWithInt:301]:  @"AD_ERROR_CACHE_VERSION_MISMATCH",
+                                 [NSNumber numberWithInt:302]:  @"AD_ERROR_CACHE_BAD_FORMAT",
+                                 [NSNumber numberWithInt:303]:  @"AD_ERROR_CACHE_NO_REFRESH_TOKEN",
+                                 [NSNumber numberWithInt:400]:  @"AD_ERROR_UI_MULTLIPLE_INTERACTIVE_REQUESTS",
+                                 [NSNumber numberWithInt:401]:  @"AD_ERROR_UI_NO_MAIN_VIEW_CONTROLLER",
+                                 [NSNumber numberWithInt:402]:  @"AD_ERROR_UI_NOT_SUPPORTED_IN_APP_EXTENSION",
+                                 [NSNumber numberWithInt:403]:  @"AD_ERROR_UI_USER_CANCEL",
+                                 [NSNumber numberWithInt:404]:  @"AD_ERROR_UI_NOT_ON_MAIN_THREAD",
+                                 [NSNumber numberWithInt:500]:  @"AD_ERROR_TOKENBROKER_UNKNOWN",
+                                 [NSNumber numberWithInt:501]:  @"AD_ERROR_TOKENBROKER_INVALID_REDIRECT_URI",
+                                 [NSNumber numberWithInt:502]:  @"AD_ERROR_TOKENBROKER_RESPONSE_HASH_MISMATCH",
+                                 [NSNumber numberWithInt:503]:  @"AD_ERROR_TOKENBROKER_RESPONSE_NOT_RECEIVED",
+                                 [NSNumber numberWithInt:504]:  @"AD_ERROR_TOKENBROKER_FAILED_TO_CREATE_KEY",
+                                 [NSNumber numberWithInt:505]:  @"AD_ERROR_TOKENBROKER_DECRYPTION_FAILED"};
+
+    return [errorCodes objectForKey:[NSNumber numberWithInt:(int)code]];
+
 }
 
 @end
