4c018a93e45bd98efd273c33fa2aead2278a1000
Jean-Marc Prieur <jmprieur@microsoft.com>
2018-10-15 16:12:20 +0200

    Archiving the sample

--- www/LogItem.js
+++
@@ -1,9 +0,0 @@
-function LogItem(item) {
-    this.message = item.message;
-    this.level = item.level;
-    this.additionalMessage = item.additionalMessage;
-    this.tag = item.tag;
-    this.errorCode = item.errorCode;
-}
-
-module.exports = LogItem;
